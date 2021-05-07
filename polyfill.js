d6fc6cefb8defe19a76cc905ce5562b943ce85f9
Ignasio M <ignasiusS@igadrigarocknroll.com>
2021-05-08 00:00:03 +0300

    feat(polyfill): add no-js class remove option

--- polyfill.js
+++ polyfill.js
@@ -1,1 +1,1 @@
-var i=new Image;i.onload=i.onerror=function(){document.body.classList.add(i.height==1?"webp":"no-webp")};i.src="data:image/webp;base64,UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==";
+document.body.classList.remove('no-js');var i=new Image;i.onload=i.onerror=function(){document.body.classList.add(i.height==1?"webp":"no-webp")};i.src="data:image/webp;base64,UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==";
